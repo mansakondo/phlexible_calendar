077ae2bd9aad840153bf29d904b0820e96bd03d7
mansakondo <mansakondo22@gmail.com>
2022-09-30 20:35:20 +0200

    Add `/lib` to `config.autoload_paths`

--- lib/phlexible_calendar/engine.rb
+++ lib/phlexible_calendar/engine.rb
@@ -5,6 +5,7 @@ module PhlexibleCalendar
   class Engine < ::Rails::Engine
     isolate_namespace PhlexibleCalendar
 
-    config.autoload_paths << File.expand_path("app/views/", "../../")
+    config.autoload_paths << File.expand_path("lib/", "#{__dir__}/../../")
+    config.autoload_paths << File.expand_path("app/views/", "#{__dir__}/../../")
   end
 end
