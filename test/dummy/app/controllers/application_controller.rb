1e92ef59fab8b7262314d2fadd585d8bf0ea4f11
mansakondo <mansakondo22@gmail.com>
2022-09-30 18:43:52 +0200

    Add the calendar

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_time_zone
+
+  private
+    def set_time_zone
+      session[:time_zone] ||= "Paris"
+      Time.zone = session[:time_zone]
+    end
 end
