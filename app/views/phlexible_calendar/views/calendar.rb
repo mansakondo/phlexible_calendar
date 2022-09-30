19f1da2bef9d3bfe21448fbdfba7ff06a30b5d58
mansakondo <mansakondo22@gmail.com>
2022-09-30 19:36:22 +0200

    Center the calendar by default

--- app/views/phlexible_calendar/views/calendar.rb
+++ app/views/phlexible_calendar/views/calendar.rb
@@ -44,41 +44,43 @@ module PhlexibleCalendar
       end
 
       def template(&block)
-        div class: "flex flex-col", data: { controller: "calendar-component" } do
-          div class: "flex ml-16 py-8"do
-            date_range.slice(0, 7).each do |day|
-              span class: "text-md text-center text-gray-600 w-[160px]" do
-                t("date.abbr_day_names")[day.wday]
+        div class: "flex justify-center" do
+          div class: "flex flex-col", data: { controller: "calendar-component" } do
+            div class: "flex ml-16 py-8"do
+              date_range.slice(0, 7).each do |day|
+                span class: "text-md text-center text-gray-600 w-[160px]" do
+                  t("date.abbr_day_names")[day.wday]
+                end
               end
             end
-          end
 
-          div class: "overflow-auto h-[540px]" do
-            div class: "flex" do
-              div class: "flex flex-col w-16" do
-                HOURS.each_with_index do |hour, i|
-                  div class: "relative h-14" do
-                    span class: "absolute bottom-12 text-xs text-gray-600" do
-                      hour
+            div class: "overflow-auto h-[540px]" do
+              div class: "flex" do
+                div class: "flex flex-col w-16" do
+                  HOURS.each_with_index do |hour, i|
+                    div class: "relative h-14" do
+                      span class: "absolute bottom-12 text-xs text-gray-600" do
+                        hour
+                      end
                     end
                   end
                 end
-              end
 
-              date_range.slice(0, 7).each do |day|
-                div class: "relative flex flex-col w-[160px]" do
-                  times_for_quarter_hours(day).each_slice(4) do |times|
-                    div class: "relative h-14 border" do
-                      times.each_with_index do |time, i|
-                        div class: "w-full", style: "height: 25%; top: #{25*i}%;", data: { time: time } do
-                          if block_given?
-                            block.call time, sorted_events.fetch(time, nil)
-                          else
-                            if (event = sorted_events.fetch(time, nil))
-                              div id: event.start_time, class: "absolute flex justify-center items-center w-full bg-gray-900 text-white text-xs z-10 rounded", style: "height: #{event.height_in_percentage}%;", draggable: true do
-                                a href: event_path(event.id) do
-                                  span do
-                                    event.name
+                date_range.slice(0, 7).each do |day|
+                  div class: "relative flex flex-col w-[160px]" do
+                    times_for_quarter_hours(day).each_slice(4) do |times|
+                      div class: "relative h-14 border" do
+                        times.each_with_index do |time, i|
+                          div class: "w-full", style: "height: 25%; top: #{25*i}%;", data: { time: time } do
+                            if block_given?
+                              block.call time, sorted_events.fetch(time, nil)
+                            else
+                              if (event = sorted_events.fetch(time, nil))
+                                div id: event.start_time, class: "absolute flex justify-center items-center w-full bg-gray-900 text-white text-xs z-10 rounded", style: "height: #{event.height_in_percentage}%;", draggable: true do
+                                  a href: event_path(event.id) do
+                                    span do
+                                      event.name
+                                    end
                                   end
                                 end
                               end
@@ -91,15 +93,15 @@ module PhlexibleCalendar
                 end
               end
             end
-          end
 
-          div class: "flex justify-around items-center mt-4" do
-            a href: url_for_previous_view, class: "flex justify-center items-center h-10 w-10 bg-gray-900 text-white rounded-full " do
-              t("simple_calendar.previous", default: "<")
-            end
+            div class: "flex justify-around items-center mt-4" do
+              a href: url_for_previous_view, class: "flex justify-center items-center h-10 w-10 bg-gray-900 text-white rounded-full " do
+                t("simple_calendar.previous", default: "<")
+              end
 
-            a href: url_for_next_view, class: "flex justify-center items-center h-10 w-10 bg-gray-900 text-white rounded-full " do
-              t("simple_calendar.next", default: ">")
+              a href: url_for_next_view, class: "flex justify-center items-center h-10 w-10 bg-gray-900 text-white rounded-full " do
+                t("simple_calendar.next", default: ">")
+              end
             end
           end
         end
