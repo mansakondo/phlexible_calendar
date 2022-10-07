13f0fbba819526cdc08f31762c7579350d4d1f28
mansakondo <mansakondo22@gmail.com>
2022-10-07 19:25:34 +0200

    Use locales to display hours

--- app/views/phlexible_calendar/views/calendar.rb
+++ app/views/phlexible_calendar/views/calendar.rb
@@ -1,37 +1,15 @@
 module PhlexibleCalendar
   module Views
     class Calendar < ApplicationView
-      HOURS = [
-        "",
-        "1 AM",
-        "2 AM",
-        "3 AM",
-        "4 AM",
-        "5 AM",
-        "6 AM",
-        "7 AM",
-        "8 AM",
-        "9 AM",
-        "10 AM",
-        "11 AM",
-        "12 AM",
-        "1 PM",
-        "2 PM",
-        "3 PM",
-        "4 PM",
-        "5 PM",
-        "6 PM",
-        "7 PM",
-        "8 PM",
-        "9 PM",
-        "10 PM",
-        "11 PM",
-        "12 PM"
-      ].freeze
-
       PARAM_KEY_BLACKLIST = :authenticity_token, :commit, :utf8, :_method, :script_name
 
       delegate_missing_to :helpers
+
+      class << self
+        def hours
+          I18n.t "phlexible_calendar.time.hours.24"
+        end
+      end
 
       attr_reader :options, :events
 
@@ -63,10 +41,10 @@ module PhlexibleCalendar
             div class: "overflow-auto h-[520px]" do
               div class: "flex" do
                 div class: "flex flex-col w-16" do
-                  HOURS.each_with_index do |hour, i|
+                  self.class.hours.each_with_index do |hour, i|
                     div class: "relative h-14" do
                       span class: "absolute bottom-12 text-xs text-gray-600" do
-                        hour
+                        hour if i > 0
                       end
                     end
                   end
